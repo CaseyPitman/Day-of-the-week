4c9570e1764fb28af018c9a03cd2a0b4f067f53e
Casey The Great <casey@caseypitman.com>
2020-07-29 17:10:28 -0500

    Basic function established. Will add 'on this day' functionality

--- app.js
+++ app.js
@@ -2,24 +2,20 @@ let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
 
 
 document.getElementById('submit').addEventListener('click', () => {
-  let inputDate = document.getElementById('input').value
-  let birthdate = new Date(inputDate);
-  let day = birthdate.getDay();
-  let dayOfWeek = days[day];
-  document.getElementById("result").innerHTML = dayOfWeek;
-  document.getElementById("input").style.display = 'none';
-  document.getElementById("submit").style.display = 'none';
-  document.getElementById("result").style.display = 'block';
-  document.getElementById("reset").style.display = 'inline-block';
+   let inputDate = document.getElementById('input').value
+   let birthdate = new Date(inputDate);
+   let day = birthdate.getDay();
+   let dayOfWeek = days[day];
+   document.getElementById('input').value = dayOfWeek;
+   document.getElementById('submit').style.display = 'none';
+   document.getElementById('reset').style.display = 'inline-block';
 });
 
 
 const init = () =>{
-	document.getElementById('input').value = "";
-  document.getElementById('input').style.display = 'inline-block';
-  document.getElementById('submit').style.display = 'inline-block';
-	document.getElementById('result').style.display = 'none';
-  document.getElementById('reset').style.display = 'none';
+   document.getElementById('input').value = "";
+   document.getElementById('submit').style.display = 'inline-block';
+   document.getElementById('reset').style.display = 'none';
 }
 
 init();
